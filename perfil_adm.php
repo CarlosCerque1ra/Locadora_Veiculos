806eadbaf53dbab1689e80162713598f8082137c
CarlosCerque1ra <carlos.a.senai02@gmail.com>
2025-04-01 15:25:35 -0300

    .

---
+++ perfil_adm.php
@@ -0,0 +1,39 @@
+<?php
+// backend
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Login</title>
+</head>
+<body class="container py-4">
+    <div class="container py4">
+        <!-- Barra de informações de usuário -->
+         <div class="row mb-4">
+            <div class="col-md-12">
+                <div class="d-flex justify-content-between aling-items-center inicio">
+                    <h1>Sistema de locadora de veiculos</h1>
+                    <d class="flex aling-items-center gap-3 user-info mx-3">
+                        <span class="user-icon">
+                        <i class="bi bi-person-circle" style="font-size:24px;"></i>
+                        </span>
+                        <!-- Bem vindo USUARIO -->
+                         <span class="welcome-text">
+                            Bem vind, <strong>administrador!</strong>
+                         </span>
+                         <!-- Botão de logout -->
+                          <a href="" class="btn btn-outline-danger d-flex align-items-center gap-1"><i class="bi bi-box-arrow-right"></i></a>
+                    </d>
+                </div>
+            </div>
+         </div>
+    </div>
+    
+</body>
+</html>
